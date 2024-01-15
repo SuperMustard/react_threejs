ccafc048467823988238a4f3e1d05365529cfa2d
SuperMustard <mr.jiahanxin@gmail.com>
2024-01-15 15:32:28 -0500

    init project and add home page

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Canvas from './canvas'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className="app transition-all ease-in">
+      <Canvas />
+      <Customizer  />
+      <Home />
+    </main>
+  )
+}
+
+export default App
