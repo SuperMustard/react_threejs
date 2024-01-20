86f021da26aec47eaff808ea761473efda1ecce3
SuperMustard <mr.jiahanxin@gmail.com>
2024-01-20 14:11:21 -0500

    add tab button, color picker and back drop

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {SketchPicker} from 'react-color'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div className='absolute left-full ml-3'>
+      <SketchPicker 
+        color = {snap.color}
+      />
+    </div>
   )
 }
 
